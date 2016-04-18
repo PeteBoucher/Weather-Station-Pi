5b7e8f606ce05a86911b300b694a3801010377aa
Pete Boucher <petebouch@gmail.com>
2016-04-18 18:09:59 +0200

    add tests for record conditions

--- webapp/sense_test.py
+++ webapp/sense_test.py
@@ -12,3 +12,11 @@ print 'Temperature in deg C:', conditions[1]
 print 'Pressure in mBar:', conditions[2]
 print 'Relative Humidity:', conditions[3]
 print 'Last reading was taken at:', conditions[4]
+
+records = sensors.record_conditions()
+print 'Record conditions:', records
+
+print 'Max temperature in deg C:', records['temp']['max']
+print 'Max pressure in mBar:', records['press']['max']
+print 'Max relative Humidity:', records['humid']['max']
+
