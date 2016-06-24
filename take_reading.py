dd10b9bd8fbce6d99aee614c2e01d60b26efdeb9
Pete Boucher <petebouch@gmail.com>
2016-06-24 10:38:44 +0200

    implement new pressure sensor

--- take_reading.py
+++ take_reading.py
@@ -1,6 +1,7 @@
 from smbus import SMBus
 import RPi.GPIO as GPIO
 import struct, array, time, io, fcntl, json
+import Adafruit_BMP.BMP085 as BMP180
 
 GPIO.setmode(GPIO.BCM)
 HDC1008_ADDR = 0x40
@@ -69,60 +70,12 @@ def read_temp(addr):
   buf = array.array('B', data)
   return (((((buf[0]<<8) + (buf[1]))/65536.0)*165.0 ) - 40.0)
 
-def read_pressure(addr):
-  # Enable event flags
-  bus.write_byte_data(addr, PT_DATA_CFG, 0x07)
-
-  # Toggel One Shot
-  setting = bus.read_byte_data(addr, CTRL_REG1)
-  if (setting & 0x02) == 0:
-      bus.write_byte_data(addr, CTRL_REG1, (setting | 0x02))
-
-  # Read sensor data
-  # print "Waiting for data..."
-    # Can throw an IOError
-  status = 0b0
-  while (status & 0x08) == 0:
-    try:
-      # Can throw an IOError
-      status = bus.read_byte_data(addr,0x00)
-    except Exception, e:
-      pass
-    time.sleep(0.5)
-    # print "status: "+bin(status)
-
-  # print "Reading sensor data..."
-  # Can throw an IOError
+def read_pressure():
   try:
-    p_data = bus.read_i2c_block_data(addr,0x01,3)
-    # t_data = bus.read_i2c_block_data(addr,0x04,2)
-    status = bus.read_byte_data(addr,0x00)
-    # print "status: "+bin(status)
-  except Exception, e:
-    raise e
-
-  p_msb = p_data[0]
-  p_csb = p_data[1]
-  p_lsb = p_data[2]
-  # t_msb = t_data[0]
-  # t_lsb = t_data[1]
-
-  pressure = (p_msb << 10) | (p_csb << 2) | (p_lsb >> 6)
-  p_decimal = ((p_lsb & 0x30) >> 4)/4.0
-
-  # celsius = t_msb + (t_lsb >> 4)/16.0
-  # fahrenheit = (celsius * 9)/5 + 32
-
-  # log_entry = "["+time.strftime('%Y%m%d %H:%M:%S%z')+"] temp:"+str(celsius)+" press:"+str((pressure+p_decimal)/100)
-  # # print log_entry
-
-  # with open('record.txt', 'a') as f:
-  #   f.write(log_entry+"\n")
-
-  # print "Pressure and Temperature at "+time.strftime('%d/%m/%Y %H:%M:%S%z')
-  # print str((pressure+p_decimal)/100)+" mBar"
-  # print str(celsius)+deg+"C"
-  return ((pressure+p_decimal)/100)
+    press_sensor = BMP180.BMP085()
+  except IOError:
+    pass
+  return press_sensor.read_pressure()
 
 anemometer_pin = 23
 vane_pin = 24
@@ -147,9 +100,10 @@ def wind_speed(cups_pin, samples=1000000):
 humid = read_humidity(HDC1008_ADDR)
 temp = read_temp(HDC1008_ADDR)
 try:
-  pressure = read_pressure(MPL3115A2_ADDR)
+  pressure = read_pressure()
 except Exception, e:
   pressure = 0
+
 wind_speed = wind_speed(anemometer_pin, 10000000)
 
 def log(temp,press,humid,wind):
